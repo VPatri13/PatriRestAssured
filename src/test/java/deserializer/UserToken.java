797a26cec9d8fa96105cec70b96dc9ed7a2329d6
val <izol@rambler.ru>
2025-02-05 17:09:23 +0300

    настраиваю каркас

---
+++ src/test/java/deserializer/UserToken.java
@@ -0,0 +1,25 @@
+package deserializer;
+
+public class UserToken {
+    private String token;
+    private String refresh_token;
+    private Integer exp;
+
+    public UserToken(Integer exp, String token, String refresh_token) {
+        this.exp = exp;
+        this.token = token;
+        this.refresh_token = refresh_token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getRefresh_token() {
+        return refresh_token;
+    }
+
+    public Integer getExp() {
+        return exp;
+    }
+}
