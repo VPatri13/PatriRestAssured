9778990eef560fdf1bba73e91e7122ae8aa96226
Валерий <v.fedorenko@sh.dunice.net>
2025-02-06 17:56:07 +0300

    Добавил

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -1,3 +1,24 @@
+import deserializer.UserToken;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class SmokeTest {
+
+    private final static String AUTH_URL = "https://dev.p-auth.hyg-core.ru/";
+
+
+
+    @Test
+    public void authorization() {
+        Specifications.installSpecification(Specifications.requestSpec(AUTH_URL), Specifications.responseSpecOk200());
+        Response response = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .
+
+    }
 
 }
---
+++ src/test/java/requestbody/loginBody.java
@@ -0,0 +1,9 @@
+package requestbody;
+
+public class loginBody {
+    private String device_id;
+
+    public loginBody(String device_id) {
+        this.device_id = device_id;
+    }
+}
