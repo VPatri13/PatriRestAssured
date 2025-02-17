a17eb151f222198fed33f7854ade068ef27e9d97
Валерий <v.fedorenko@sh.dunice.net>
2025-02-17 14:22:40 +0300

    Перевел на тестовый стенд

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -11,9 +11,19 @@ import static io.restassured.RestAssured.given;
 
 public class SmokeTest {
 
-    private final static String AUTH_URL = "https://dev.p-auth.hyg-core.ru";
+    private final static String AUTH_URL = "https://test.hyg-core.ru";
     private final static String DEVICE_ID = "12345678-1234-1234-1234-123456789123";
-
+    private final static String AUTHENTICATION_TOKEN = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk2MDlkOGZlLWFjN2It" +
+            "NDdiZC05NmQ0LTY1MTk1NjExMzg1MCIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyLCJkZXZpY2VfaWQiOiIxMjM0NTYtMTIzN" +
+            "C0xMjM0LTEyMzQtMTIzNDU2Nzg5MTIzIiwibmFtZSI6InZlcm9uaWthMTQiLCJ0eXBlIjoidXNlciIsInN1YiI6ImZyb250Iiw" +
+            "iaWF0IjoxNzM2OTYzMDU2LCJleHAiOjE3NjgwNjcwNTZ9.ILLX7eq1No3GnoK7fqGdce-BjDugbwI8RLV61BJzRrFMGMwUDI3R" +
+            "4JzLwRoPSvxgAiTnPW_omt4mcJCkMt60zWHFIb3wma92uWQrEz02EIHcvQWD9wtihkjPRkYDaEo3MGlafsekMAn5pPpdIe6xnz" +
+            "6ZRwJY-kUV3M_rT9jhbQ4uQM-jEYMdOx1vFVWu5H3Loh3S2nMspbrlb5qgWb5gRPF8ERincd9ZwHpr-zAKVKhYe6JRfVp6F3VK" +
+            "2DrAm_WJ4P_jgI4vDfPi0HiVUviMDbIQQbagpuES_nKK7imdb59z12xFQC1o2juAHJXirqYXkrjpjs1IaLxA5itGrsaRGgO9fh" +
+            "evkCf0UZ1oCbUn174Kh9Erzhb4nYgLiw4ezOJ6Jd_pBA13POE6HODO26JVp7bROlZnMIn3egiuoq24d4HINAowZ4jiZQeQrge3" +
+            "jP143VDIMzXOAo-MKqCu5Sg72bAET5bjjU-Mw7_syU3W1b84R5harJzOzovxuvXkgmjh6I8h6H33W0zZs1QvHKmhUUXk91YqlVL" +
+            "A1mUNLGlp8CaFgZylV5ib6PxubrG6LsdN6-aKCQ7QdK33qqBMPLp3rDL9stkBNZBji8znsa1Q6-ocuK6mjB3iykvrXN4rVE6M4h" +
+            "iLYHPxbXemC1SpkLIo5QOmxr4SqUQWt16U8N2Ft1A";
     private final static String ENDPOINT_LOGIN_PHONE = "/login_phone/{phone}";
 
     private final static String USER_PHONE = "79000000001";
@@ -33,6 +43,7 @@ public class SmokeTest {
         LoginBody loginBody = new LoginBody(DEVICE_ID);
         SuccessLogin successLogin = given()
                 .contentType(ContentType.JSON)
+                .header("Authorization", AUTHENTICATION_TOKEN)
                 //.pathParam("{phone}", USER_PHONE)
                 .body(loginBody)
                 .when()
@@ -54,6 +65,7 @@ public class SmokeTest {
         LoginBody loginBody = new LoginBody(DEVICE_ID);
         UserToken userToken = given()
                 .contentType(ContentType.JSON)
+                .header("Authorization", AUTHENTICATION_TOKEN)
                 .queryParams("code", CMC_CODE)
                 .body(loginBody)
                 .post("/login_phone/" + USER_PHONE)
