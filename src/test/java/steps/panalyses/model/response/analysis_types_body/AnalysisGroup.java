7a28c0e243c96bb0947a3ef474a3d3c00aa7ddb3
Валерий <v.fedorenko@sh.dunice.net>
2025-03-18 17:56:55 +0300

    тест

--- src/test/java/steps/panalyses/model/response/analysis_types_body/AnalysisGroup.java
+++ src/test/java/steps/panalyses/model/response/analysis_types_body/AnalysisGroup.java
@@ -5,4 +5,24 @@ public class AnalysisGroup {
     public boolean is_default;
     public int id;
 
+    public AnalysisGroup(String name, boolean is_default, int id) {
+        this.name = name;
+        this.is_default = is_default;
+        this.id = id;
+    }
+
+    public AnalysisGroup() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isIs_default() {
+        return is_default;
+    }
 }
--- src/test/java/steps/panalyses/model/response/analysis_types_body/Item.java
+++ src/test/java/steps/panalyses/model/response/analysis_types_body/Item.java
@@ -10,5 +10,39 @@ public class Item {
     public AnalysisGroup analysis_group;
     public ArrayList<LaboratoryAnalysis> laboratory_analyses;
 
+    public Item(String name, boolean is_default, int analysis_group_id, int id, AnalysisGroup analysis_group, ArrayList<LaboratoryAnalysis> laboratory_analyses) {
+        this.name = name;
+        this.is_default = is_default;
+        this.analysis_group_id = analysis_group_id;
+        this.id = id;
+        this.analysis_group = analysis_group;
+        this.laboratory_analyses = laboratory_analyses;
+    }
 
+    public Item() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<LaboratoryAnalysis> getLaboratory_analyses() {
+        return laboratory_analyses;
+    }
+
+    public AnalysisGroup getAnalysis_group() {
+        return analysis_group;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAnalysis_group_id() {
+        return analysis_group_id;
+    }
+
+    public boolean isIs_default() {
+        return is_default;
+    }
 }
